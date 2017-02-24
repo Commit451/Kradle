7b8feff24ab1b8ab5391097d5e05d2a80dc7a931
John Carlson <jawnnypoo@gmail.com>
2017-02-24 12:57:38 -0600

    Logger

--- src/main/java/com/commit451/kradle/App.kt
+++ src/main/java/com/commit451/kradle/App.kt
@@ -1,17 +1,22 @@
 package com.commit451.kradle
 
+import org.slf4j.LoggerFactory
 import spark.Spark
 
 object App {
 
+    private val LOGGER = LoggerFactory.getLogger(App::class.java)
+
     @JvmStatic
     fun main(args: Array<String>) {
         Spark.port(8080)
-        Spark.get("/hello") { request, response ->
+        Spark.get("*") { request, response ->
+            LOGGER.warn("GET with path: ${request.pathInfo()}")
             "Hello World!"
         }
-        Spark.put("/") { request, response ->
+        Spark.put("*") { request, response ->
             // Update something
+            LOGGER.warn("PUT with path: ${request.pathInfo()}")
             response.status(200)
         }
     }
